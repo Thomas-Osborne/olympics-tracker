6544fe10db16d7b801f295bfd7d783a2b9d66e2d
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-08-19 17:13:57 +0100

    Frontend API calls added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Country from './countries/Country';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/api/countries');
+                setData(response.data.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+              setLoading(false)
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+
+    return (
+        <div className="App">
+            <h1>Paris 2024 Data</h1>
+            {data 
+                ? data.map(dataItem => (
+                    <Country key={dataItem.id} data={dataItem} />
+                ))
+                : <p>Error fetching data.</p>
+            }
+        </div>
+    );
 }
-
-export default App
---
+++ frontend/src/countries/Country.jsx
@@ -0,0 +1,15 @@
+export default function Country(props) {
+  return (
+    <div key={props.data.id}>
+      <h2>{props.data.name}</h2>
+      <img src={props.data.flag_url} alt={props.data.name} width="30" />
+      <p>Continent: {props.data.continent}</p>
+      <p>Gold Medals: {props.data.gold_medals}</p>
+      <p>Silver Medals: {props.data.silver_medals}</p>
+      <p>Bronze Medals: {props.data.bronze_medals}</p>
+      <p>Total Medals: {props.data.total_medals}</p>
+      <p>Rank: {props.data.rank}</p>
+      <p>Total Medals Rank: {props.data.rank_total_medals}</p>
+    </div>
+  )
+}
